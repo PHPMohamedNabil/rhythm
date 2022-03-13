eca72d680311865ae6d6188ecde78ac9af4144d0
unknown <mohamedn085@gmail.com>
2022-03-13 05:11:48 +0200

    changing in database seeding file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Category;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Category::factory(100)->create();
+        DB::table('users')->insert([
+            'name' => 'mohamed',
+            'username'=>'Admin',
+            'email' =>'mohamedn085@gmail.com',
+            'password' => Hash::make('12345678')
+        ]);
+
+        DB::table('roles')->insert([
+            'name' =>'Admin',
+            'description' => 'Full Access'
+        ]);
+
+        DB::table('permissions')->insert([
+            'role_id' => 1,
+            'permissions' =>'{"categories":{"can-add":"on","can-edit":"on","can-view":"on","can-delete":"on","can-list":"on","can-publish":"on"},"questions":{"can-add":"on","can-edit":"on","can-view":"on","can-delete":"on","can-list":"on","can-publish":"on"},"pages":{"can-add":"on","can-edit":"on","can-view":"on","can-delete":"on","can-list":"on","can-publish":"on"},"templates":{"can-add":"on","can-edit":"on","can-view":"on","can-delete":"on","can-list":"on","can-publish":"on"},"users":{"can-add":"on","can-edit":"on","can-view":"on","can-delete":"on","can-list":"on","can-publish":"on"},"permissions":{"can-add":"on","can-edit":"on","can-view":"on","can-delete":"on","can-list":"on","can-publish":"on"},"roles":{"can-add":"on","can-edit":"on","can-view":"on","can-delete":"on","can-list":"on","can-publish":"on"},"walls":{"can-add":"on","can-edit":"on","can-view":"on","can-delete":"on","can-list":"on","can-publish":"on"},"system_links":{"can-add":"on","can-edit":"on","can-view":"on","can-delete":"on","can-list":"on","can-publish":"on"}}'
+        ]);
     }
 }
