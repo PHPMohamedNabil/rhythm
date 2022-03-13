3ce07920befeed4cd9d81f5f3d746e79ac24ddfd
unknown <mohamedn085@gmail.com>
2022-03-13 16:01:28 +0200

    edit question file name

--- app/Models/question.php
+++ app/Models/question.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 
-class question extends Model
+class Question extends Model
 {
     use HasFactory;
 
